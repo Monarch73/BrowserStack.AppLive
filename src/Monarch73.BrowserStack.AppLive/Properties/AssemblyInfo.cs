ff51082e325da66558cfd6cfdb0d8156ad1b8251
Niels Hüsken <NielsH@ranger.de>
2022-10-21 13:47:30 +0200

    added test project

--- src/Monarch73.BrowserStack.AppLive/Properties/AssemblyInfo.cs
+++ src/Monarch73.BrowserStack.AppLive/Properties/AssemblyInfo.cs
@@ -2,5 +2,7 @@
 // Licensed under the unlicense license. See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Monarch73.BrowserStack.AppLive.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010075a64988d4102cf897b701f7862ab93fab876249163f9d1eded88c03ab1b3bfacd6f4c5c373a8b5d1799e7b909e2fb984fd14495f3fc027402e6b3f5b508eb111fe329fed3d0b26a26cc0edc384a34ea3ff0ec7e99069083825c203d98c04af2149966b3f6e2fe20486fe743658f7b7f1d0b0cf3b46a878833db155d3c77b7b6")]
---
+++ tests/Monarch73.BrowserStack.AppLive.Tests/TestImplementation.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Monarch73, 2022. All rights reserved.
+// Licensed under the unlicense license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monarch73.BrowserStack.AppLive;
+
+namespace Monarch73.BrowserStack.AppLive.Tests;
+
+/// <summary>
+/// Implements various tests.
+/// </summary>
+[TestClass]
+public class TestImplementation
+{
+    /// <summary>
+    /// Tests basic connectivity.
+    /// </summary>
+    [TestMethod]
+    public void TestConnectivity()
+    {
+        var test = new BrowserStackAppLiveClient(string.Empty, string.Empty);
+        using (var abc = new HttpClient())
+        {
+            var test2 = new BrowserStackAppLiveClient(string.Empty, string.Empty, abc);
+        }
+    }
+}
