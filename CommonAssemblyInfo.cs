8ee38b8099e48846c1adff3f436f9dad6bb8f9f7
Niels Hüsken <NielsH@ranger.de>
2022-10-21 12:41:10 +0200

    1st compiling version

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Monarch73, 2022. All rights reserved.
+// Licensed under the unlicense license. See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Monarch73.BrowserStack.AppLive")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
---
+++ src/Monarch73.BrowserStack.AppLive/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Monarch73, 2022. All rights reserved.
+// Licensed under the unlicense license. See the LICENSE file in the project root for full license information.
+
+using System;
+
+[assembly: CLSCompliant(true)]
